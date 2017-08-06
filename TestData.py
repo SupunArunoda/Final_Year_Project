173adbd5b8d78101e47c4a52edf1fdd454fb82ed
SupunArunoda <supunarunoda@gmail.com>
2017-08-07 02:13:16 +0530

    Add spark but not as a server

--- TestData.py
+++ TestData.py
@@ -1,11 +1,12 @@
 from InputData import LobsterData
+from spark.InputData import Spark
 from model.cluster.KMeans import Kmeans
 
 def test_read_single_date_files():
     message_file = './data/AAPL_2012-06-21_34200000_37800000_message_50.csv'
 
-    lob = LobsterData()
-    lob.run_data_process(message_file=message_file)
+    lob = Spark()
+    lob.read_data(message_file=message_file)
 def analyze_model():
     message_file='./output/vecotrozed_AMZN_level_50_data.csv'
     kml = Kmeans()
