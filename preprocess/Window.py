93b7ba8b43c4a590ccc69539c5f3f24b2eaeb8a6
SupunArunoda <supunarunoda@gmail.com>
2017-08-12 00:21:20 +0530

    Add volume average calculation static

--- preprocess/Window.py
+++ preprocess/Window.py
@@ -14,6 +14,10 @@ class Window:
         self.price_average_list = []
         self.count_order_list = [[0 for _ in range(2)] for _ in range(3)]
 
+        self.order_volume_list = [[0 for _ in range(2)] for _ in range(3)]
+        self.volume_average_list = []
+        self.count_order_volume_list = [[0 for _ in range(2)] for _ in range(3)]
+
         self.session = read_csv(session_file)
         self.regular_list=self.get_regular_time()
 
@@ -29,7 +33,7 @@ class Window:
         return reg_list
 
     def get_time_frame(self,order):
-        const_time_gap=datetime.timedelta(0, 240)#set time window value
+        const_time_gap=datetime.timedelta(0, 5)#set time window value
         temp_trasact_time=DUp.parse(order.transact_time)
         for i in range(0,len(self.regular_list),2):
             if(temp_trasact_time>=self.regular_list[i] and temp_trasact_time<=self.regular_list[i+1]):
@@ -41,11 +45,18 @@ class Window:
                         self.check_order(order=order)
 
                         index=str(self.temp_time)+str('$$')+str(order.transact_time)
-                        self.price_average_list.append(index)
-                        self.get_average()
+                        self.volume_average_list.append(index)
+                        self.get_average_volume()
                         self.attributes = self.attributes.append(DataFrame(
-                            {'time_index': self.price_average_list[0], 'new_order_buy': self.price_average_list[1], 'new_order_sell': self.price_average_list[2], 'cancel_order_buy': self.price_average_list[3],
-                             'cancel_order_sell': self.price_average_list[4], 'execute_order_buy': self.price_average_list[5], 'execute_order_sell': self.price_average_list[6]}, index=[0]), ignore_index=True);
+                            {'time_index': self.volume_average_list[0], 'new_order_buy': self.volume_average_list[1],
+                             'new_order_sell': self.volume_average_list[2],
+                             'cancel_order_buy': self.volume_average_list[3],
+                             'cancel_order_sell': self.volume_average_list[4],
+                             'execute_order_buy': self.volume_average_list[5],
+                             'execute_order_sell': self.volume_average_list[6]}, index=[0]), ignore_index=True);
+                        # self.attributes = self.attributes.append(DataFrame(
+                        #     {'time_index': self.price_average_list[0], 'new_order_buy': self.price_average_list[1], 'new_order_sell': self.price_average_list[2], 'cancel_order_buy': self.price_average_list[3],
+                        #      'cancel_order_sell': self.price_average_list[4], 'execute_order_buy': self.price_average_list[5], 'execute_order_sell': self.price_average_list[6]}, index=[0]), ignore_index=True);
 
                         self.remove_values()
                         self.temp_time=0
@@ -60,11 +71,14 @@ class Window:
 
     def check_order(self,order):
         if order.execution_type==0:#new order check
-            self.count_order(order=order,type=0)
+            self.count_order_volume(order=order, type=0)
+            #self.count_order(order=order,type=0)
         elif order.execution_type==4:#cancel order check
-            self.count_order(order=order,type=1)
+            self.count_order_volume(order=order, type=1)
+            #self.count_order(order=order,type=1)
         elif order.execution_type == 15:#execution order
-            self.count_order(order=order, type=2)
+            self.count_order_volume(order=order, type=2)
+            #self.count_order(order=order, type=2)
 
 
     def count_order(self,order,type):
@@ -75,6 +89,21 @@ class Window:
         elif (order.side == 2):#sell order check
             self.order_price_list[type][1] += order.value
             self.count_order_list[type][1] += 1
+
+    def count_order_volume(self, order, type):
+        if (order.side == 1):  # buy order check
+            if(type!=2):
+                self.order_volume_list[type][0] += order.visible_size
+            else:
+                self.order_volume_list[type][0] += order.executed_qty
+            self.count_order_volume_list[type][0] += 1
+
+        elif (order.side == 2):  # sell order check
+            if (type != 2):
+                self.order_volume_list[type][0] += order.visible_size
+            else:
+                self.order_volume_list[type][0] += order.executed_qty
+            self.count_order_volume_list[type][0] += 1
 
     def get_average(self):
 
@@ -87,6 +116,16 @@ class Window:
                 else:
                     self.price_average_list.append(float(0))
 
+    def get_average_volume(self):
+
+        for i in range(len(self.order_volume_list)):
+            for j in range(len(self.order_volume_list[i])):
+                if(self.count_order_volume_list[i][j]!=0):
+                    temp_average=self.order_volume_list[i][j]/self.count_order_volume_list[i][j]
+                    self.volume_average_list.append(round(temp_average,4))
+
+                else:
+                    self.volume_average_list.append(float(0))
 
 
     def remove_values(self):
